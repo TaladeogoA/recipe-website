0af690c6659feb04e69caf2fe8b27d7f4cc54a1d
TaladeogoA <ataladeogo@gmail.com>
2024-11-09 13:47:44 +0100

    Project and routes setup

---
+++ src/app/recipes/page.tsx
@@ -0,0 +1,9 @@
+const RecipesPage = () => {
+  return (
+    <div className="min-h-screen p-8 flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Recipes Page</h1>
+    </div>
+  );
+};
+
+export default RecipesPage;
