f5799d013cacb551aa2adbf869219b42f575c8fa
TaladeogoA <ataladeogo@gmail.com>
2024-12-05 05:39:12 +0100

    Dynamically import Awards, Reviews, and Instagram sections for improved performance and loading behavior

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { Page } from "@/components/layouts";
-
 import {
-  AwardsSection,
   CommunitySection,
   HeroSection,
-  ReviewsSection,
   StorySection,
 } from "@/components/sections/about";
-import { InstagramSection } from "@/components/sections/shared/instagram-section";
 import {
   AwardsSkeleton,
   CommunitySkeleton,
@@ -17,10 +13,35 @@ import {
   ReviewsSkeleton,
   StorySkeleton,
 } from "@/components/skeletons/about";
+import dynamic from "next/dynamic";
 import { Suspense, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+
+const ReviewsSection = dynamic(
+  () =>
+    import("@/components/sections/about/reviews-section").then((mod) => ({
+      default: mod.ReviewsSection,
+    })),
+  { ssr: false }
+);
+
+const AwardsSection = dynamic(
+  () =>
+    import("@/components/sections/about/awards-section").then((mod) => ({
+      default: mod.AwardsSection,
+    })),
+  { ssr: false }
+);
+
+const InstagramSection = dynamic(
+  () =>
+    import("@/components/sections/shared/instagram-section").then((mod) => ({
+      default: mod.InstagramSection,
+    })),
+  { ssr: false }
+);
 
 const AboutPage = () => {
   const [reviewSlider, setReviewSlider] = useState<Slider | null>(null);
