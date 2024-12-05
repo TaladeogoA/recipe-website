02e0e481e267d5ae8d0bfca5a59529da0ebf9ff5
TaladeogoA <ataladeogo@gmail.com>
2024-12-05 06:54:41 +0100

    Reduce loading skeletons from 6 to 3 in SearchResults component for improved performance

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -34,7 +34,7 @@ function SearchResults() {
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
               {isLoading
-                ? [...Array(6)].map((_, i) => (
+                ? [...Array(3)].map((_, i) => (
                     <div key={i} className="flex flex-col">
                       <Skeleton className="aspect-[4/3] w-full mb-4" />
                       <Skeleton className="h-[2px] w-12 mb-4" />
